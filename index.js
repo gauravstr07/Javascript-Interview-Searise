48dafe50e22ccac59d0c1eccbcee0944f11acadc
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-01 12:30:49 +0530

    Objects in Javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+let arrName = [
+  {
+    name: "Gaurav",
+    age: 22,
+  },
+  {
+    name: "Unknown",
+    age: 23,
+  },
+];
+
+console.log(
+  arrName.indexOf({
+    name: "gauravstr",
+    pass: 2680,
+  })
+);
